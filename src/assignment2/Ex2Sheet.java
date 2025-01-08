754088bbda91cd4e6b05d22220b129cc74eb08bb
Yakovemny <yakovemn@gmail.com>
2025-01-08 13:11:44 +0200

    PartB

--- src/assignment2/Ex2Sheet.java
+++ src/assignment2/Ex2Sheet.java
@@ -1,5 +1,4 @@
 package assignment2;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- src/assignment2/Ex2SheetTest.java
+++ src/assignment2/Ex2SheetTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Ex2SheetTest {
-/*
+
     @Test
     void value() {
 
@@ -19,18 +19,7 @@ class Ex2SheetTest {
     void testGet() {
     }
 
-    @Test
-    void xCell(){
-        assertEquals(5 , Ex2Sheet.xCell("F13"));
-        assertEquals(-1 , Ex2Sheet.xCell("AA1"));
-        assertEquals(-1 , Ex2Sheet.xCell("BB1"));
-        assertEquals(-1 , Ex2Sheet.xCell("CC1"));
-    }
-    @Test
-    void yCell(){
-        assertEquals(13 , Ex2Sheet.yCell("F13")); //returns -1
-        assertEquals(7 , Ex2Sheet.yCell("A7"));
-    }
+
     @Test
     void width() {
 
@@ -55,17 +44,6 @@ class Ex2SheetTest {
     @Test
     void extractCellReferences() {
     }
-/*
-    @Test
-    void initializeArr() {
-        int[][] arr = new int[10][10];
-        Ex2Sheet.initializeArr(arr);
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                assertEquals(-1 , arr[i][j]);
-            }
-        }
-    }
 
 
 
@@ -81,5 +59,5 @@ class Ex2SheetTest {
     void testEval() {
     }
 
- */
+
 }
